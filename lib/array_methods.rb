658a97e6c2ed014ef2d074c4249897912e8ddf1b
Ben Manekin <ben.manekin@gmail.com>
2019-01-25 19:15:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,12 +7,14 @@ def using_sort(array)
   array.sort
 end
 
-def using_reverse(array)
 
+def using_reverse(array)
+  array.reverse
 end
 
-def using_first(array)
 
+def using_first(array)
+  array.first
 end
 
 def using_last(array)
